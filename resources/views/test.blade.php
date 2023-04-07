5d5361e1c7e267d92ead3a2ecbaa0c03d997a35d
alexx108 <alexx1088@gmail.com>
2023-04-07 13:29:29 +0600

    first commit

---
+++ resources/views/test.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>КДЛ Олимп</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+<header>
+<div class="container header_container">
+<div class="header__address">
+<img class="" src="{{ asset('images/logo.png') }}" alt="logo">
+    <p> г. Омск<br>
+        ул. Карла Маркса, 45
+    </p>
+</div>
+</div>
+
+
+
+
+
+
+
+</header>
+</body>
+</html>
