be994ca2b579a5692d72d99520460358e3666e63
alexx108 <alexx1088@gmail.com>
2023-04-07 15:25:14 +0600

    add styles to header

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -10,21 +10,38 @@
 </head>
 <body>
 <header>
-<div class="container header_container">
-<div class="header__address">
-<img class="" src="{{ asset('images/logo.png') }}" alt="logo">
-    <p> г. Омск<br>
-        ул. Карла Маркса, 45
-    </p>
-</div>
-</div>
-
-
-
-
-
-
-
+    <div class="container flex space-between items-center space-between">
+        <div class="header__address ">
+            <img class="logo" src="{{ asset('images/logo.png') }}" alt="logo">
+            <p class="text_sf text"> г. Омск<br>
+                ул. Карла Маркса, 45
+            </p>
+        </div>
+        <div class="header__phone ">
+            <a href="tel:+79835200554" class="text header__phone-number">+7 983 520 05 54</a>
+            <a href="#" class="button button_green">Whatsapp</a>
+        </div>
+    </div>
 </header>
+<section class="main section">
+    <div class="container container-section">
+        <h1 class="text_gradient text_analyzes text_sf">Достоверные<br> медицинские<br> анализы</h1>
+
+        <div class="section__address ">
+            <a href="#" class="button button_fill_blue ">Анализы</a>
+            <a href="#" class="button button_blue" >Как проехать</a>
+        </div>
+        <div class="container  text_sf_regular container_properties">
+            <p><b>3000+</b><br>видов анализов</p>
+            <p>Швейцарские<br>анализаторы Roche</p>
+            <p>Профессиональные<br>медсестры</p>
+        </div>
+
+    </div>
+</section>
+
+
+
+
 </body>
 </html>
